fe316fe2098a746abaa1ae388b3ea872c4319bf3
C1oudust <amerkiousy@gmail.com>
2019-11-29 22:11:53 +0800

    Update Database

---
+++ otenki_girl/Model/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace otenki_girl.Model
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string Address { get; set; }
+        public string Name { get; set; }
+        public string Reason { get; set; }
+        public string AddTime { get; set; }
+    }
+}
